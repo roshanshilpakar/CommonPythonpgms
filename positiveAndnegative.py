2407580fa5e1af276cffe13efab8661e8b22192b
roshanshilpakar <roshan.shilpakar.2@gmail.com>
2020-06-17 08:49:06 +0530

    initial

---
+++ positiveAndnegative.py
@@ -0,0 +1,8 @@
+x= float(input('Please enter the number : \n'))
+
+if x>0:
+    print("This is a POSITIVE number")
+elif x < 0:
+    print("This is a NEGATIVE number")
+else:
+    print("The number is ZERO")
